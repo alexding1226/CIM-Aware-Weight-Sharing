b034ae5d3d6e289c4b3f43cfc9fc9e4634511ecf
alexding1226 <55429899+alexding1226@users.noreply.github.com>
2024-07-31 17:08:37 +0800

    debug util check_distance

--- utils.py
+++ utils.py
@@ -319,10 +319,10 @@ def train_epochs(model, device, train_dataloader, val_dataloader, eval_dataloade
             torch.save(model.state_dict(), checkpoint[:-3]+"_%i.pt"%epoch)
 
         if ((epoch-args.start_share_epoch) % args.share_every == 0) and (epoch >= args.start_share_epoch):
-            weight_grad_share.check_distance(model=model, qkv_ratio=args.qkv_ratio-0.01, fc1_ratio=args.fc1_ratio-0.01, fc2_ratio=args.fc2_ratio-0.01,macro_width=args.macro_width,args=args,distance_boundary=args.check_distance_value)
+            weight_grad_share.check_distance(model=model, macro_width=args.macro_width,args=args,distance_boundary=args.check_distance_value)
             print("start sharing")
             weight_grad_share.weight_share_all(model=model, qkv_ratio=args.qkv_ratio, fc1_ratio=args.fc1_ratio, fc2_ratio=args.fc2_ratio,macro_width=args.macro_width,args=args,distance_boundary=0.01, set_mask = False)
-            weight_grad_share.check_distance(model=model, qkv_ratio=args.qkv_ratio-0.01, fc1_ratio=args.fc1_ratio-0.01, fc2_ratio=args.fc2_ratio-0.01,macro_width=args.macro_width,args=args,distance_boundary=args.check_distance_value)
+            weight_grad_share.check_distance(model=model, macro_width=args.macro_width,args=args,distance_boundary=args.check_distance_value)
 
             print("Shared Checkpoint saved.")
             torch.save(model.state_dict(), checkpoint[:-3]+"_%i_shared.pt"%epoch)
